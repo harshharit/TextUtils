7bfa4830cf3ae937c666da5ae3de6f37fb91ed95
harshharit <50503002+harshharit@users.noreply.github.com>
2023-03-14 17:24:26 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './style.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import About from './components/About';
 
 
 export default function App() {
   return (
     <>
     <Navbar/>
-    <Textform heading="Enter the text below to analyze"/>
+    {/* <Textform heading="Enter the text below to analyze"/> */}
+    <About/>
     </>
   );
 }
